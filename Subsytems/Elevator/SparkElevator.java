4e207d364da1d2a0321e1fda60e0a9cf40bc92f5
Powerlax <raovishruth@gmail.com>
2025-01-16 19:53:00 -0800

    fixed open and closed loop clash

--- Subsytems/Elevator/SparkElevator.java
+++ Subsytems/Elevator/SparkElevator.java
@@ -13,10 +13,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class SparkElevator extends Elevator {
     SparkMax leadMotor;
     SparkMax followMotor;
+    boolean usePID;
 
-    public SparkElevator(MotorConfig motorConfig, FollowerConfig followerConfig, FFConfig ffConfig, boolean tuningMode, String subsystemName) {
+    public SparkElevator(MotorConfig motorConfig, FollowerConfig followerConfig, FFConfig ffConfig, boolean tuningMode, boolean usePID, String subsystemName) {
         super(ffConfig, tuningMode, subsystemName);
 
+        this.usePID = usePID;
         leadMotor = motorConfig.createSparkMax();
         leadMotor.getEncoder().setPosition(0.0);
         followMotor = followerConfig.createSparkMax();
@@ -26,11 +28,13 @@ public class SparkElevator extends Elevator {
     @Override
     public void periodic() {
         super.tuning.updatePID(leadMotor);
-        leadMotor.getClosedLoopController().setReference(
-            super.setpoint.get(), 
-            ControlType.kPosition, 
-            ClosedLoopSlot.kSlot1, 
-            super.feedforward.calculate(getEncoderPos(), 0.0));
+        if (usePID) {
+            leadMotor.getClosedLoopController().setReference(
+                super.setpoint.get(), 
+                ControlType.kPosition, 
+                ClosedLoopSlot.kSlot1, 
+                super.feedforward.calculate(getEncoderPos(), 0.0));
+        }
         SmartDashboard.putNumber("Elevator lead motor pos", getEncoderPos());
         if (followMotor != null) {
             SmartDashboard.putNumber("Elevator follow motor pos", followMotor.getEncoder().getPosition());
