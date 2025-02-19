b79a2ca223cb135fc880fc15b01d31610dadeba3
Aditya9246 <G.Aditya2409@hotmail.com>
2025-02-19 00:32:09 -0800

    implemented TalonPivot

--- src/main/java/poplib/subsytems/pivot/TalonPivot.java
+++ src/main/java/poplib/subsytems/pivot/TalonPivot.java
@@ -1,8 +1,7 @@
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.spark.ClosedLoopSlot;
-import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkMax;
+package poplib.subsytems.pivot;
 
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import poplib.control.FFConfig;
 import poplib.motor.FollowerConfig;
@@ -15,6 +14,7 @@ public class TalonPivot extends Pivot {
     @SuppressWarnings("unused")
     private final TalonFX followerMotor;
     private final PIDTuning pid;
+    private final PositionDutyCycle position;
 
     public TalonPivot(MotorConfig leadConfig, FollowerConfig followerConfig, double gearRatio, FFConfig ffConfig, AbsoluteEncoderConfig absoluteConfig, boolean tuningMode, String subsytemName) {
         super(ffConfig, absoluteConfig, tuningMode, subsytemName);
@@ -26,6 +26,10 @@ public class TalonPivot extends Pivot {
         };
 
         pid = leadConfig.genPIDTuning("Pivot Motor " + subsytemName, tuningMode);
+        position = new PositionDutyCycle(0.0);
+        position.withSlot(leadMotor.getClosedLoopSlot().getValue());
+
+        leadMotor.setPosition(0.0);
 
         resetToAbsolutePosition();
     }
@@ -38,6 +42,10 @@ public class TalonPivot extends Pivot {
         return Math.abs(leadMotor.getPosition().getValueAsDouble() - setpoint);
     }
 
+    public void updatePID() {
+        leadMotor.setControl(position.withPosition(super.setpoint.get()).withFeedForward(super.ff.getKg()));
+    }
+
     @Override
     public void log() {
         super.log();
@@ -47,14 +55,7 @@ public class TalonPivot extends Pivot {
     @Override
     public void periodic() {
         pid.updatePID(leadMotor);
-
-        // TODO: Move to mutable units
-        leadMotor.getClosedLoopController().setReference(
-            setpoint.get(), 
-            ControlType.kPosition,
-            ClosedLoopSlot.kSlot0,
-            ff.calculate(leadMotor.getPosition().getValueAsDouble(), 0)
-        );
+        leadMotor.setControl(position.withPosition(super.setpoint.get()).withFeedForward(super.ff.getKg()));
     }
 
     @Override
