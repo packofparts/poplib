9e36ffec83889a3393df7a667bfedce96a8125da
SaranyaA20 <146662808+SaranyaA20@users.noreply.github.com>
2025-02-19 19:02:12 -0800

    cleaned up camera class code

--- src/main/java/poplib/sensors/camera/Camera.java
+++ src/main/java/poplib/sensors/camera/Camera.java
@@ -2,31 +2,24 @@ package poplib.sensors.camera;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Vector;
-
-import javax.swing.text.html.HTML.Tag;
 
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import poplib.sensors.camera.CameraConfig;
 import poplib.sensors.camera.StdDevStategy;
 
@@ -56,9 +49,9 @@ public class Camera {
                 PhotonTrackedTarget target = result.getBestTarget();
                 Optional<Pose3d> pose = layout.getTagPose(target.getFiducialId());
                 if (pose.isPresent()) {
-                ret = Optional.of(new Pose2d(
-                    new Translation2d(target.getBestCameraToTarget().getX(), target.getBestCameraToTarget().getY()), 
-                    Rotation2d.fromRadians(pose.get().getRotation().getZ())));
+                    ret = Optional.of(new Pose2d(
+                        new Translation2d(target.getBestCameraToTarget().getX(), target.getBestCameraToTarget().getY()), 
+                        Rotation2d.fromRadians(pose.get().getRotation().getZ())));
                 }
            }
          }
@@ -68,19 +61,22 @@ public class Camera {
 
     public Optional<EstimatedRobotPose> getEstimatedPose(Pose2d currPose) {
         if (!camera.isConnected()) {
-            DriverStation.reportError("Camera named: " + config.cameraName + " is not connected!!!!!!!!", false);
-            // the above code should save to the log file that you can view in the DS Log Viewer
+            DriverStation.reportError("Camera named: " + config.cameraName + " is not connected!", false);
             return Optional.empty();
         }
+
         poseEstimator.setReferencePose(currPose);
+        
         Optional<EstimatedRobotPose> visionEst = Optional.empty();
+
         for (PhotonPipelineResult change : camera.getAllUnreadResults()) {
             visionEst = poseEstimator.update(change);
-            updateStdDevs(visionEst, change.getTargets());
         }
+
         if (visionEst.isPresent() && isValidPose(visionEst.get())) {
             return visionEst;
         }
+
         return Optional.empty();
     }
 
@@ -88,6 +84,8 @@ public class Camera {
         return currStdDevs;
     }
 
+    // TODO: Test implementation
+    // Currently not in use
     private void updateStdDevs(Optional<EstimatedRobotPose> visionEst, List<PhotonTrackedTarget> targets) {
         if (visionEst.isEmpty()) {
             currStdDevs = singleTagStdDevs;
@@ -101,7 +99,7 @@ public class Camera {
                     if (tagPose.isPresent()) {
                         numTags++;
                         avg += tagPose.get().toPose2d().getTranslation().getDistance(
-                        visionEst.get().estimatedPose.toPose2d().getTranslation());
+                            visionEst.get().estimatedPose.toPose2d().getTranslation());
                     }
                 } else if (config.stdDevStategy == StdDevStategy.AMBIGUITY) {
                     avg += target.getPoseAmbiguity();
@@ -117,12 +115,12 @@ public class Camera {
                 }
                 if (config.stdDevStategy == StdDevStategy.DISTANCE) {
                     if (numTags == 1 && avg > 4) {
-                        stdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);  // tag too unreliable
+                        stdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
                     } else {
                         stdDevs = stdDevs.times(1 + (avg * avg / 30));
                     }
                 } else if (config.stdDevStategy == StdDevStategy.AMBIGUITY) {
-                    if (avg > 0.4) {    // ur cooked lil bro
+                    if (avg > 0.4) {  
                         stdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
                     } else {
                         stdDevs = stdDevs.times(1 + (avg * avg * 30));
@@ -136,19 +134,25 @@ public class Camera {
     private boolean isValidPose(EstimatedRobotPose pose) {
         List<PhotonTrackedTarget> targets = pose.targetsUsed;
         double max_dist = 0;
+
         for (PhotonTrackedTarget target : targets) {
             Optional<Pose3d> tagPose = poseEstimator.getFieldTags().getTagPose(target.getFiducialId());
             if (tagPose.isPresent()) {
                 max_dist = Math.max(max_dist, tagPose.get().toPose2d().getTranslation().
-                getDistance(pose.estimatedPose.toPose2d().getTranslation()));
+                    getDistance(pose.estimatedPose.toPose2d().getTranslation()));
             }
         }
+
         if (max_dist > config.poseDistanceThreshold) {
+            System.out.println("Target Rejected Based on Distance Threshold");
             return false;
         }
-        if (targets.size() == 1) {
-            return targets.get(0).getPoseAmbiguity() < config.poseAmbiguityThreshold;
+
+        if (targets.size() == 1 && targets.get(0).getPoseAmbiguity() < config.poseAmbiguityThreshold) {
+            System.out.println("Target Rejected Based on Ambiguity");
+            return false;
         }
+
         return true;
     }
 }
