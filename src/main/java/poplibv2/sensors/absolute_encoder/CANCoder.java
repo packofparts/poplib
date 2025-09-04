48658869e4fef075f59fe3f14150dd2ab2a2a7be
Powerlax <raovishruth@gmail.com>
2025-09-03 21:36:13 -0700

    fixed absolute encoders

--- src/main/java/poplibv2/sensors/absolute_encoder/CANCoder.java
+++ src/main/java/poplibv2/sensors/absolute_encoder/CANCoder.java
@@ -6,12 +6,18 @@ import com.ctre.phoenix6.signals.SensorDirectionValue;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.Units;
+import poplibv2.misc.CanIdRegistry;
 
 public class CANCoder {
     private CANcoder encoder;
 
+    /**
+     * Creates a new CANCoder (absolute encoder) using the CANCoderConfig
+     * @param config the config to be used
+     */
     public CANCoder(CANCoderConfig config) {
         encoder = new CANcoder(config.id, config.CANBus);
+        CanIdRegistry.getRegistry().registerCanId(config.id);
 
         CANcoderConfiguration sensorConfig = new CANcoderConfiguration();
         sensorConfig.MagnetSensor.SensorDirection = SensorDirectionValue.valueOf(config.inversion ? 1 : 0);
@@ -20,6 +26,10 @@ public class CANCoder {
         encoder.getConfigurator().apply(sensorConfig);
     }
 
+    /**
+     * Gets the position the CANCoder is displaying
+     * @return The position as a Rotation2d in rotations.
+     */
     public Rotation2d getPosition() {
         return Rotation2d.fromRotations(encoder.getAbsolutePosition().getValue().in(Units.Rotations));
     }
--- src/main/java/poplibv2/sensors/absolute_encoder/CANCoderConfig.java
+++ src/main/java/poplibv2/sensors/absolute_encoder/CANCoderConfig.java
@@ -8,6 +8,13 @@ public class CANCoderConfig {
     public final Rotation2d offset;
     public final boolean inversion;
 
+    /**
+     * Creates a new Config for a CANCoder.
+     * @param id The CAN Id of the CANCoder.
+     * @param CANBus The CANBus of the CANCoder
+     * @param offset The offset of the CANCoder, used to align your swerve wheels.
+     * @param inversion Whether or not the encoder should be inverted.
+     */
     public CANCoderConfig(int id, String CANBus, Rotation2d offset, boolean inversion) {
         this.id = id;
         this.CANBus = CANBus;
